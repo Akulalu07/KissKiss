992adcc87cfb173773935429631543094e1568af
Egor <vinograd1020gm@gmail.com>
2025-10-04 15:26:39 +0300

    Added endpoint

--- src/main/java/net/kiskiss/app/api/Controller.java
+++ src/main/java/net/kiskiss/app/api/Controller.java
@@ -1,5 +1,6 @@
 package net.kiskiss.app.api;
 
+import net.kiskiss.app.model.Data;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @PostMapping
-    public void getPoint(@RequestBody Point point){
-        System.out.println(point);
+    @PostMapping("/api/route")
+    public void getPoint(@RequestBody Data point){
+        System.out.println(point.toString());
     }
 
 }
--- src/main/java/net/kiskiss/app/model/Data.java
+++ src/main/java/net/kiskiss/app/model/Data.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 
 @Getter
 @AllArgsConstructor
+@lombok.Data
 public class Data {
 
     private final Point point;
