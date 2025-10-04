ecb3d7c977838a8dcc217aa36c67e59f0182e598
Egor <vinograd1020gm@gmail.com>
2025-10-04 15:35:49 +0300

    Added cross origin

--- src/main/java/net/kiskiss/app/api/Controller.java
+++ src/main/java/net/kiskiss/app/api/Controller.java
@@ -1,11 +1,13 @@
 package net.kiskiss.app.api;
 
 import net.kiskiss.app.model.Data;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
 
     @PostMapping("/api/route")
